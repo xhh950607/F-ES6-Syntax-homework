935afd0cac3244c2d57abd55acfefaded6118133
xuhuihui <494655043@qq.com>
2020-09-11 18:03:41 +0800

    practice 2

--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,11 @@
-const inject
+const inject = (items, sections) => {
+    sections.sort((a, b) => a.index - b.index)
+
+    let i = 0;
+    sections.forEach(section => {
+        items.splice(section.index + i, 0, section.content);
+        i++;
+    });
+    return items;
+}
 export { inject };

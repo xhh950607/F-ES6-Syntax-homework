a9c7555c0e17ef35600c3598c84fe49b0eff7995
xuhuihui <494655043@qq.com>
2020-09-11 17:34:33 +0800

    practice 1

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,10 @@
-const parseData
+const parseData = ({ data, column }) => {
+    const keys = column.map(col => col.name);
+    
+    return data.map(values => {
+        const res = {};
+        keys.forEach((key, index) => res[key] = values[index]);
+        return res;
+    })
+}
 export { parseData };

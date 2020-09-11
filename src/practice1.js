e378ee712e91ca08251be853144d6fd8eda1f4b5
xuhuihui <494655043@qq.com>
2020-09-11 17:46:23 +0800

    practice 1 (try another method)

--- src/practice1.js
+++ src/practice1.js
@@ -1,10 +1,15 @@
 const parseData = ({ data, column }) => {
     const keys = column.map(col => col.name);
-    
+
     return data.map(values => {
-        const res = {};
-        keys.forEach((key, index) => res[key] = values[index]);
-        return res;
+        const entries = keys.map((key, index) => [key, values[index]]);
+        return Object.fromEntries(entries);
     })
+
+    // return data.map(values => {
+    //     const res = {};
+    //     keys.forEach((key, index) => res[key] = values[index]);
+    //     return res;
+    // })
 }
 export { parseData };
